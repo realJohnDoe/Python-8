f75cde63b24e18d797742ec6138b5e8ee8a9af0e
Robert Storlind <carlrobert@users.noreply.github.com>
2023-03-18 12:22:07 +0100

    Step02

--- Blocks/blocks.py
+++ Blocks/blocks.py
@@ -4,13 +4,18 @@ import pgzrun
 WIDTH = 20 * 14
 HEIGHT = 20 * 25
 
+grid_x_count = 10
+grid_y_count = 18
+
+inert = []
+
 
 # Funktioner (def) här nedanför
 def draw():
     screen.fill((255, 255, 255))
 
-    for y in range(18):
-        for x in range(10):
+    for y in range(grid_y_count):
+        for x in range(grid_x_count):
             block_size = 20
             block_draw_size = block_size - 1
             screen.draw.filled_rect(
@@ -23,5 +28,9 @@ def draw():
 
 
 # Kod för att starta appen här nedanför
+for y in range(grid_y_count):
+    inert.append([])
+    for x in range(grid_x_count):
+        inert[y].append(' ')
 
 pgzrun.go()  # måste vara sista raden
